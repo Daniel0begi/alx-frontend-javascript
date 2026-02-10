000f45c2a9e16fc9aef20b3cc7fafc9ae562cdb1
simonmumina <simonmumina2000@gmail.com>
2026-02-10 13:12:58 +0300

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -29,8 +29,8 @@ console.log(director1);
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 } 
-const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
-  return `${firstName[0]}. ${lastName}`;
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName}. ${lastName}`;
 }
 console.log(printTeacher('John', 'Doe'));
 interface StudentClassConstructor {
